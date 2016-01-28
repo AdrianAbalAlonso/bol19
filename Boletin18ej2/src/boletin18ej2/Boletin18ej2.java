48a47eaaeede9ff049f8c1de93c024eb6d6722aa
aabalalonso <aabalalonso@atanasoff16.danielcastelao.org>
2016-01-28 09:39:20 +0100

    commit correjido de unos errores

---
+++ Boletin18ej2/src/boletin18ej2/Boletin18ej2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin18ej2;
+
+/**
+ *
+ * @author aabalalonso
+ */
+public class Boletin18ej2 {
+
+   
+    public static void main(String[] args) {
+        Metodos obx=new Metodos();
+        obx.CargarArray();
+        obx.pedirNota();
+        obx.NotaMedia();
+        obx.calcularAprobados();
+        
+        
+    }
+    
+}
